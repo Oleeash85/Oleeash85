a66bac3299c16ba19ca5f8b7a49acd598252a3d1
Ashley Arlene Martin-Gatewood <104944457+Oleeash85@users.noreply.github.com>
2022-08-20 14:12:04 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,30 +1,38 @@
-var Height, width, color;
+const size = document.getElementById('#sizeSelector');
+const height = document.getElementById('#gridHeight');
+const width = document.getElementById('#gridHeight');
+const color = document.querySelector('#colorSelector');
+const canvas = document.getElementById('#pixelCanvas');
 
-$('#sizeSelector').submit(function(event) {
+
+$('size').submit(function(event) {
     event.preventDefault();
-    height = $('#gridHeight').val();
-    width = $('#gridWidth').val();
+    height.val();
+    width.val();
     makeGrid(height, width);
-
 })
 
-function makeGrid(a, b) {
-    $('tr').remove();
+function makeGrid(x, y) {
+    document.createElement('tr').remove();
 
-    for (var c = 1; c <= a; c++) {
-        $('#pixelCanvas').append('<tr id=table' + c + '></tr');
-        for (var d = 1; d <= b; d++) {
-            $('#table' + c).append('<td></td>');
+    for (var c = 0; c < y; c++) {
+        document.getElementById('#pixelCanvas').append('<tr id=table' + c + '></tr');
+        for (var d = 0; d < y; d++) {
+            document.getElementById('#table' + c).append('<td></td>');
         }
 
     }
-    $('td').click(function addcolor() {
-        color = $('#colorSelector').val();
 
-        if ($(this).attr('style')) {
-            $(this).removeAttr('style')
+    addEventListener('td').click(function color() {
+        color = document.getElementById('#colorSelector').val();
+
+        if (document.getElementById('#pixelCanvas').attr('style')) {
+            (document.getElementById('#pixelCanvas').removeAttribute('style'))
+
         } else {
             $(this).attr('style', 'background-color:' + color);
         }
+
     })
+    
 }
